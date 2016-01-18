e02e61b2b5b3c810a63a9f76338ad8d8da74b649
Rhonda Impey <rhonda_impey@hotmail.co.uk>
2016-01-18 11:21:02 +0000

    Added rotating cubes to replace the flipping tiles on the front page

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,12 +2,4 @@ $(document).foundation();
 
 $(document).ready(function() {
     
-    $('.tile-wrapper').hover(
-        function(){
-            $(this).find('.tile').addClass('the-flip');
-        },
-        function(){
-            $(this).find('.tile').removeClass('the-flip');
-        }
-    );
 });
